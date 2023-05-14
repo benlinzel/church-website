239cfd44ce3ed19a0a32cfa19f22fad6cb72e678
Ben Linzel <benlinzel@icloud.com>
2023-05-14 16:06:55 -0400

    Revert "Remove unneeded pages, add analytics"
    This reverts commit ff9f9befedd3490150a9f110bdb23ef078ddcab8.

--- nextjs-app/src/pages/_app.tsx
+++ nextjs-app/src/pages/_app.tsx
@@ -1,13 +1,9 @@
-import { Analytics } from "@vercel/analytics/react";
-import "../styles/global.css";
+import '../styles/global.css';
 
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <>
-    <Component {...pageProps} />
-    <Analytics />
-  </>
+  <Component {...pageProps} />
 );
 
 export default MyApp;
