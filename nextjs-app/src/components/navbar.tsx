e02d5ad395060ffe955a701f8c32165db99ff4e1
Ben Linzel <benlinzel@icloud.com>
2023-10-14 10:39:23 -0400

    More Cleanup

--- nextjs-app/src/components/navbar.tsx
+++ nextjs-app/src/components/navbar.tsx
@@ -10,56 +10,6 @@ const Navbar = (props: any) => {
       )}
       <ul className="z-20 flex flex-row">
         {' '}
-        {/* <li className="group relative">
-          <p>About</p>
-          <div className="absolute -right-6 hidden w-28 rounded-lg bg-black/40 p-2 group-hover:block">
-            <ul className="top-0 space-y-2">
-              <li>
-                <Link href="/our-values" className="text-white">
-                  Our Values
-                </Link>
-              </li>
-              <li>
-                <Link href="/our-team" className="text-white">
-                  Our Team
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </li>
-        <li className="group relative">
-          <p>Ministries</p>
-          <div className="absolute -right-6 hidden w-28 rounded-lg bg-black/40 p-2 group-hover:block">
-            <ul className="top-0 space-y-2">
-              <li>
-                <Link
-                  href="small-groups"
-                  className="whitespace-nowrap text-white"
-                >
-                  Small Groups
-                </Link>
-              </li>
-              <li>
-                <Link href="missions" className="whitespace-nowrap text-white">
-                  Missions
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="https://wgc-library.vercel.app/"
-                  className="text-white"
-                >
-                  Library
-                </Link>
-              </li>
-              <li>
-                <Link href="/giving" className="text-white">
-                  Giving
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </li> */}
         <li>
           <span className="group relative inline-block text-white drop-shadow-[0_20px_20px_rgba(0,0,0,0.7)]">
             <button className="inline-flex items-center rounded px-4 font-normal group-hover:text-amber-500">
@@ -74,14 +24,6 @@ const Navbar = (props: any) => {
                   About WGC
                 </Link>
               </li>
-              {/* <li className="">
-                <Link
-                  className="whitespace-no-wrap block bg-stone-800 py-2 px-4 text-white hover:to-stone-600 hover:text-amber-500"
-                  href="/our-values"
-                >
-                  Our Values (remove)
-                </Link>
-              </li> */}
               <li className="">
                 <Link
                   className="whitespace-no-wrap block bg-stone-800 px-4 py-2 text-white hover:bg-stone-700  hover:text-amber-500"
@@ -147,14 +89,6 @@ const Navbar = (props: any) => {
                   Small Groups
                 </Link>
               </li>
-              {/* <li className="">
-                <Link
-                  className="whitespace-no-wrap block bg-stone-800 py-2 px-4 text-white hover:bg-stone-700   hover:text-amber-500"
-                  href="/missions"
-                >
-                  Missions
-                </Link>
-              </li> */}
               <li className="">
                 <Link
                   className="whitespace-no-wrap block rounded-b-md border-t border-white/20 bg-stone-800 px-4 py-2 text-right text-white hover:bg-stone-700   hover:text-amber-500"
