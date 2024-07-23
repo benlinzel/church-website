e80108f38610c47d1f8fe5efa17998b474d04079
Ben Linzel <benlinzel@icloud.com>
2024-07-22 21:21:36 -0400

    fix background color

--- nextjs-app/src/pages/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function Layout({ children }: {children: any}) {
-  return (
-    <>
-      <main className="bg-zinc-900 text-zinc-100 min-h-screen">{children}</main>
-    </>
-  )
-}
